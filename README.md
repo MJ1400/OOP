af732b5476dbb1974429efcf14c6b20b53c3fa84
Marten Jürgens <55079581+MJ1400@users.noreply.github.com>
2020-04-19 23:14:39 +0300

    Added homework

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# OOP
-Java OOP
+#OOP
+
+#Homework
+Week 1: ✔️
+Week 2: ✔️
+Week 3: ✔️
+Week 4: ✔️
+Week 5: ✔️
+Week 6: ❓
+Week 7: ❓
+Week 8: ❌
--- w03_klassikomplekt/Test.java
+++ w03_klassikomplekt/Test.java
@@ -1,10 +1,25 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.HashMap;
 
 public class Test {
 
   public static void main(String[] args) {
     ArrayList<String> list = new ArrayList<String>();
+    HashSet <String> hashlist = new HashSet<String>();
+    HashMap<String, String> fullnames = new HashMap<String, String>();
 
+    hashlist.add("Eesti");
+    hashlist.add("Soome");
+    hashlist.add("Rootsi");
+    System.out.println(hashlist);
+    System.out.println("Kas hashlist sisaldab Eestit?: " + hashlist.contains("Eesti"));
+    System.out.println("-----------------------------------");
+    fullnames.put("Marten", "Jyrgens");
+    fullnames.put("Karmo", "Pihlakas");
+    fullnames.put("Rasmus", "Tamm");
+    System.out.println(fullnames);
+    System.out.println("-----------------------------------");
     list.add("Hello");
     list.add("world");
     list.add(String.valueOf(5));
