82ca4a50eb8ddb67b7006027a6040638944a8ece
Marten Jürgens <55079581+MJ1400@users.noreply.github.com>
2020-05-07 19:05:10 +0300

    W9 done

--- w09_tunnirakenduse_lahendus/Tund.java
+++ w09_tunnirakenduse_lahendus/Tund.java
@@ -4,14 +4,15 @@ import java.util.List;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
-public class Main {
+class Tund {
 
     public static void main(String[] args) throws IOException {
         int test2[] = new int[2];
         List<String> listA = new ArrayList<>();
         List<Integer> listB = new ArrayList<>();
-        Logger logger = Logger.getLogger(Main.class.getName());
+        Logger logger = Logger.getLogger(Tund.class.getName());
         FileHandler fh = new FileHandler("log.txt");
         logger.addHandler(fh);
         try{
@@ -31,16 +32,25 @@ public class Main {
             
         }
 
-        listA.add("TEST1");
-        listA.add("TEST2");
-        listA.add("TEST3");
-        listA.add("TEST4");
+        listA.add("TEST12342");
+        listA.add("TEST25346");
+        listA.add("TEST35555555555555");
+        listA.add("TEST43463467");
 
         listB.add(1);
         listB.add(2);
         listB.add(3);
         listB.add(4);
         listB.add(5);
+        listB.add(6);
+        listB.add(7);
+        listB.add(8);
+        listB.add(9);
+        listB.add(10);
+        listB.add(7);
+        listB.add(7);
+        listB.add(3);
+        listB.add(8);
 
         for(String s : listA) {
             System.out.println(s);
@@ -54,19 +64,22 @@ public class Main {
         System.out.println("---------------------------------------");
         List<String> lowercases = listA.stream().map(String::toLowerCase).collect(Collectors.toList());
         System.out.println(lowercases);
+        System.out.println("---------------------------------------");
+        listB.stream().map(i -> i * 2).forEach(System.out::println);
+        System.out.println("---------------------------------------");
+        listA.stream().map(s -> s.length()).forEach(System.out::println);
 
+        IntStream.of(1, 3, 5, 7, 10, 5, 12, 5, 26, 34).map(e -> e * e).forEach(System.out::println);
 
+        IntStream.range(1, 100).filter(i -> i % 2 == 0).forEach(System.out::println);
+
+        boolean answer1 = listB.stream().allMatch(num-> num > 3); 
+        System.out.println(answer1);
+
+        boolean answer2 = listB.stream().anyMatch(num-> num > 3); 
+        System.out.println(answer2);
         
     }
 }
 
-    // public static void LogError(Exception e) throws IOException {
-            
-            
-	// 		// FileWriter fw = new FileWriter("log.txt", true);
-    //         fw.write(e.toString());
-    //         fw.write(System.getProperty("line.separator"));
-	// 		fw.close();
-    //     }
-        
-    // }
+
