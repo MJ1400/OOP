e9447eedb93a0f17024ebbb3d0750a8d92a1b3e9
Marten Jürgens <55079581+MJ1400@users.noreply.github.com>
2020-05-07 19:09:16 +0300

    W2 Done

--- w02_klass/Kodune/Koduneyl.java
+++ w02_klass/Kodune/Koduneyl.java
@@ -7,6 +7,7 @@ public class Koduneyl {
         Furniture lamp = new Furniture(3, 1);
         System.out.println("Diivani k6rgus on: " + couch.height + " ja diivani laius on " + couch.width);
         System.out.println("Lambi k6rgus on: " + lamp.height + " ja lambi laius on " + lamp.width);
+        
 
     }
     
