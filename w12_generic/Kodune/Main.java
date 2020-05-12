326f64a4886349449bc7c3f4f301060275fea1e5
Marten Jürgens <55079581+MJ1400@users.noreply.github.com>
2020-05-12 06:38:13 +0300

    W12 Done

---
+++ w12_generic/Kodune/Main.java
@@ -0,0 +1,9 @@
+package Kodune;
+
+public class Main {
+    public static void main(String[] args) {
+        Functions<Integer> fn = new Functions<>(4);
+        //Functions<String> fn2 = new Functions<>("aaa");
+        System.out.println(fn.ruut());
+    }
+}
